16ff6a23a05bcb01a848236d350818aae07832dd
Ryan Yeh <>
2018-01-24 16:46:13 -0500

    completed issue #1

---
+++ src/components/pages/TodoPage.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React, { Component } from 'react'
+import mastermind from './../../mastermind'
+
+
+export default class TodoPage extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1 className="placeholder-page-label"> Todo Page </h1>
+      </div>
+    )
+  }
+}
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,3 +1,4 @@
 export { default as LoginPage } from './LoginPage'
 export { default as HomePage } from './HomePage'
 export { default as PageNotFound } from './404Page'
+export { default as TodoPage } from './TodoPage'
