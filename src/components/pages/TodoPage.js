5949566f49756f75034fe19b18fde2179efadf90
Warren Neil Gordon <neil@Warrens-MBP.home>
2018-01-26 16:26:44 -0500

    add todos to TodoPage and render todos in the table

--- src/components/pages/TodoPage.js
+++ src/components/pages/TodoPage.js
@@ -8,35 +8,40 @@ class TodoPage extends Component {
 
   render() {
 
+    const { todos } = this.props
+
     return (
       <div>
         <h1 className="placeholder-page-label"> Todo Page </h1>
         <table className="ui celled striped table todos-table">
           <thead>
             <tr>
-              <th>
-                Title
-              </th>
-              <th>
-                Description
-              </th>
-              <th>
-                Completed
-              </th>
+              <th> Title </th>
+              <th> Description </th>
+              <th> Completed </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>
-                Get weird
-              </td>
-              <td>
-                Let's get weird
-              </td>
-              <td>
-
-              </td>
+              <td> Get weird </td>
+              <td> Lets get weird </td>
+              <td> </td>
             </tr>
+
+            {
+              Object.keys(todos).map(id => {
+                const todo = todos[id]
+                const { title, description, isComplete } = todo
+                return(
+                  <tr key={id}>
+                    <td> { title } </td>
+                    <td> { description } </td>
+                    <td> { isComplete } </td>
+                  </tr>
+                )
+              })
+            }
+            
           </tbody>
         </table>
       </div>
