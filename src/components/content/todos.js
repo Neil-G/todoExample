2470cadabad98eb6d5242c05d55f03408aa41143
Ryan Yeh <>
2018-01-31 14:25:34 -0500

    Created content folder and made an array of todos to be populated in the todos table

---
+++ src/components/content/todos.js
@@ -0,0 +1,17 @@
+export default {
+    1: {
+      title: 'get weird',
+      description: 'getting weird',
+      completed: true,
+    },
+    2: {
+      title: 'get loud',
+      description: 'we get loud in dis',
+      completed: false,
+    },
+    3: {
+      title: 'get figgity',
+      description: 'tryna get figgity???',
+      completed: true,
+    }
+}
